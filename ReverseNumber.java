ace605a3732f30b26c39cf1b1e0aea9c97c536f4
venkat-pathipati <venkat.pathipati@gmail.com>
2025-09-07 16:13:21 +0530

    Create ReverseNumber.java

---
+++ ReverseNumber.java
@@ -0,0 +1,14 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int number = 12345;  // number to reverse
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;    // get last digit
+            reversed = reversed * 10 + digit; // add it to reversed
+            number = number / 10;      // remove last digit
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+    }
+}
